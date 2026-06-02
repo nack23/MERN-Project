8669e19b9b171cfb55908206d21659a8f7652d0e
Qais Alam <cosmicwaveglaxy@gmail.com>
2026-06-03 00:56:50 +0530

    Update rateLimiter.js

--- backend/middleware/rateLimiter.js
+++ backend/middleware/rateLimiter.js
@@ -1,1 +1,22 @@
+const rateLimit = require("express-rate-limit");
 
+const signupLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 5,
+    message: {
+        message: "Too many signup attempts. Try again later."
+    }
+});
+
+const loginLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 10,
+    message: {
+        message: "Too many login attempts. Try again later."
+    }
+});
+
+module.exports = {
+    signupLimiter,
+    loginLimiter
+};
